857c7d2b432eb3ec7027dd2310320516264f4f75
Difizz10 <146772299+Difizz10@users.noreply.github.com>
2023-12-16 22:52:38 +0300

    Add files via upload

---
+++ repository/package_repository.go
@@ -0,0 +1,47 @@
+package repository
+
+import (
+	"context"
+
+	"github.com/
+	"github.com/
+	"go.mongodb.org/mongo-driver/bson"
+)
+
+type taskRepository struct {
+	database   mongo.Database
+	collection string
+}
+
+func NewTaskRepository(db mongo.Database, collection string) domain.PacketRepository {
+	return &taskRepository{
+		database:   db,
+		collection: collection,
+	}
+}
+
+func (tr *taskRepository) Create(c context.Context, task *domain.Packet) error {
+	collection := tr.database.Collection(tr.collection)
+
+	_, err := collection.InsertOne(c, task)
+
+	return err
+}
+
+func (tr *taskRepository) FetchAll(c context.Context) ([]domain.Packet, error) {
+	collection := tr.database.Collection(tr.collection)
+
+	var tasks []domain.Packet
+
+	cursor, err := collection.Find(c, bson.M{})
+	if err != nil {
+		return nil, err
+	}
+
+	err = cursor.All(c, &tasks)
+	if tasks == nil {
+		return []domain.Packet{}, err
+	}
+
+	return tasks, err
+}
---
+++ usecase/package_usecase.go
@@ -0,0 +1,32 @@
+package usecase
+
+import (
+	"context"
+	"time"
+
+	"github.com/
+)
+
+type taskUsecase struct {
+	taskRepository domain.PacketRepository
+	contextTimeout time.Duration
+}
+
+func NewTaskUsecase(taskRepository domain.PacketRepository, timeout time.Duration) domain.PacketUsecase {
+	return &taskUsecase{
+		taskRepository: taskRepository,
+		contextTimeout: timeout,
+	}
+}
+
+func (tu *taskUsecase) Create(c context.Context, task *domain.Packet) error {
+	ctx, cancel := context.WithTimeout(c, tu.contextTimeout)
+	defer cancel()
+	return tu.taskRepository.Create(ctx, task)
+}
+
+func (tu *taskUsecase) FetchAll(c context.Context) ([]domain.Packet, error) {
+	ctx, cancel := context.WithTimeout(c, tu.contextTimeout)
+	defer cancel()
+	return tu.taskRepository.FetchAll(ctx)
+}
